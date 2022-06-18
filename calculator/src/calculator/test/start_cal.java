4e1bcaf9d52ce23ff5d01f5156693038c26da000
lovee9523 <noeun1102@gmail,com>
2022-06-18 17:34:11 +0900

    화면 창 생성

--- calculator/src/calculator/test/start_cal.java
+++ calculator/src/calculator/test/start_cal.java
@@ -1,11 +1,22 @@
 package calculator.test;
 
-public class start_cal {
+import javax.swing.JFrame;
 
+import lombok.Getter;
+import lombok.Setter;
+
+public class start_cal extends JFrame{
+
+	public start_cal() {
+		setSize(500,640);
+		setLayout(null); //그림그릴떄 절대적 위치 아닌 상대적 위치 가능
+		setVisible(true);// 화면 창 윈도우에 띄우는 기능
+	}
+	
 	public static void main(String[] args) {
 		//계산기 시작
-		System.out.println("main에서 출력");
-
+		new start_cal();
+		
 	}
 
 }
